cebb9411c1bcb760edee9903acc646bd8d00725d
Seth Keenan <seth.j.keenan@gmail.com>
2024-05-01 03:01:29 -0500

    NTS disreguard

--- DungeonForge/src/main/java/Main.java
+++ DungeonForge/src/main/java/Main.java
@@ -3,6 +3,17 @@ import characterManagement.classType;
 
 public class Main {
 
+	//Add level too all of the classes
+	//This will implement, import character feature (current character)
+	
+	//Finish Stats to make character builder with builder feature
+	
+	//Finish inline display (Terminal stats display) with display feature
+	
+	//Add a way to save character data to a csv file with save feature
+	
+	//Read any csv formatted correctly with load feature
+	
 	public static void main(String[] args) {
 		CharacterFactory factory = new CharacterFactory();
 		
